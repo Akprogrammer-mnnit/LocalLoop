034d08c96ee5895ed9ea694539ec81d9e770f557
Ayan Khandelwal <khandelwalayan11@gmail.com>
2026-02-03 21:13:34 +0530

    Fix header issue in render proxy

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,6 +25,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
+app.set('trust proxy', 1);
 app.use(compression({
     filter: (req, res) => {
         if (req.headers['x-no-compression']) {
